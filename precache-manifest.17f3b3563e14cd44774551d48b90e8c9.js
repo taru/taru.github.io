6b2235a814aab926b9074eb38d6d10b97ea6bba1
taru <39840910+taru@users.noreply.github.com>
2019-01-17 02:51:22 -0500

    Updates

---
+++ precache-manifest.17f3b3563e14cd44774551d48b90e8c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "dbefdd0a13f02e81d919",
+    "url": "/robofriends/static/js/main.dbefdd0a.chunk.js"
+  },
+  {
+    "revision": "1ee35225728cefb1e598",
+    "url": "/robofriends/static/js/1.1ee35225.chunk.js"
+  },
+  {
+    "revision": "dbefdd0a13f02e81d919",
+    "url": "/robofriends/static/css/main.36a03864.chunk.css"
+  },
+  {
+    "revision": "1ee35225728cefb1e598",
+    "url": "/robofriends/static/css/1.77fa5400.chunk.css"
+  },
+  {
+    "revision": "62d81085f13c407b9068699f4f877a80",
+    "url": "/robofriends/index.html"
+  }
+];
